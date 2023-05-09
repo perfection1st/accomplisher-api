8777554c287d5591355ed6120cb1501fc93b3619
perfection1st <107283558+perfection1st@users.noreply.github.com>
2023-05-09 15:20:41 -0500

    Adds accomplisher app db schema

--- accomplisher_schema.sql
+++ accomplisher_schema.sql
@@ -1,2 +1,41 @@
-DROP DATABASE IF EXISTS accomplisher_data;
-CREATE DATABASE accomplisher_data;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS todos;
+DROP TABLE IF EXISTS workouts;
+DROP TABLE IF EXISTS meals;
+
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(50),
+  last_name VARCHAR(50),
+  created_on DATE,
+  username VARCHAR(50),
+  password VARCHAR(255),
+);
+
+CREATE TABLE todos (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER REFERENCES users(id),
+  task TEXT NOT NULL,
+  due_date DATE,
+  important BOOLEAN default false,
+  completed BOOLEAN DEFAULT false
+);
+
+CREATE TABLE workouts (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER REFERENCES users(id),
+  workout_date DATE,
+  workout_type TEXT,
+  duration_minutes INTEGER,
+  calories_burned INTEGER
+);
+
+CREATE TABLE meals (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER REFERENCES users(id),
+  meal_date DATE,
+  meal_type TEXT,
+  meal_name TEXT,
+  calories INTEGER
+);
